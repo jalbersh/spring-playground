365e37b7dd1096db9bbef3fc07b346ac1cccb8d8
James Albersheim <james@albersheim.com>
2018-08-15 07:14:54 -0600

    add /5 endpoint

--- src/main/java/com/galvanize/jalbersh/springplayground/controller/LessonsController.java
+++ src/main/java/com/galvanize/jalbersh/springplayground/controller/LessonsController.java
@@ -1,9 +1,11 @@
 package com.galvanize.jalbersh.springplayground.controller;
 
 import com.galvanize.jalbersh.springplayground.model.Lesson;
-import com.galvanize.jalbersh.springplayground.model.LessonRepository;
+import com.galvanize.jalbersh.springplayground.repository.LessonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/lessons")
@@ -21,9 +23,22 @@ public class LessonsController {
         return this.repository.findAll();
     }
 
+    @GetMapping("/5")
+    public Optional<Lesson> get5() {
+        return this.repository.findById(5l);
+    }
+
     @PostMapping("")
     public Lesson create(@RequestBody Lesson lesson) {
         return this.repository.save(lesson);
     }
 
 }
+
+/*
+insert into lessons (title, delivered_on) values ('Requests and Responses', '2017-02-05');
+insert into lessons (title, delivered_on) values ('SQL', '2017-04-06');
+insert into lessons (title, delivered_on) values ('Bellman Ford', '2017-05-02');
+insert into lessons (title, delivered_on) values ('Some Other Lesson', '2017-05-02');
+insert into lessons (title, delivered_on) values ('JPA', '2017-05-02');
+ */
