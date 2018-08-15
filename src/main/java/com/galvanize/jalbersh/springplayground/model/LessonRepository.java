fcdd8d15b28b79b5e331ff83a6601de3cec2b842
James W Albersheim <macbookpro2018@Jamess-MacBook-Pro-2018.local>
2018-08-14 20:08:31 -0600

    add /5 endpoint

--- src/main/java/com/galvanize/jalbersh/springplayground/model/LessonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.galvanize.jalbersh.springplayground.model;
-
-import com.galvanize.jalbersh.springplayground.model.Lesson;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LessonRepository extends CrudRepository<Lesson, Long> {
-
-}
-
